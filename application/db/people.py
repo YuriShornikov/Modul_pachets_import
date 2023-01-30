7fd80f2b7d16eebfaa77283881e6e5ae121828ef
Yuri Shornikov <dbonlyfun@gmail.com>
2023-01-30 23:29:07 +0300

    question

--- application/db/people.py
+++ application/db/people.py
@@ -1,5 +1,13 @@
 
 
+list_employees = []
 
 def get_employees():
-    pass
+    global list_employees
+    name = input('Введите имя сотрудника: ')
+    surname = input('Введите фамилию сотрудника: ')
+    list_employees.append(name + ' ' + surname)
+
+
+get_employees()
+print(list_employees)
